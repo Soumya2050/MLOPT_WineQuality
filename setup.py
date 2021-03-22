03df6ec46ba56b15902de5623ed80c46a4a6beab
Soumya Sethi <soumyasethi@pop-os.localdomain>
2021-03-23 01:11:49 +0530

    Predection working

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    description="its a wine Q package", 
-    author="c17hawke", 
+    description="its a wine Q package",
+    author="soumya2050",
     packages=find_packages(),
     license="MIT"
 )
-
